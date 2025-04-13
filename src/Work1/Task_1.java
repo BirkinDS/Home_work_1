14df5ee36a6b43dcbf18c385997d8d5fe93ae83e
BirkinDS <myclefable@gmail.com>
2025-04-13 20:31:50 +0300

    Задачи для проверки

--- src/Work1/Task_1.java
+++ src/Work1/Task_1.java
@@ -1,5 +1,17 @@
 package Work1;
 
 public class Task_1 {
+    public static void main(String[] args) {
 
+        //Задаем значение переменных.
+        int b = 2;
+        int c = 2;
+
+        //Пишем формулу для вычисления значений
+        int a = 4*(b+c-1)/2;
+
+        //Выводим результат вычисления в конлось
+        System.out.println(a);
+
+    }
 }
--- src/Work1/Task_2.java
+++ src/Work1/Task_2.java
@@ -1,4 +1,17 @@
 package Work1;
 
 public class Task_2 {
+    public static void main(String[] args) {
+        //Задаем значение переменных
+        int n = 26;
+        //Пишем формулу для вычисления значений
+        int sum1 = n / 10;
+        int sum2 = n % 10;
+        int sum3 = sum1 + sum2;
+        //Выводим результат вычисления
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+
+    }
 }
